c25a8701c4d6a11929bd9976b29bd01f7e8fe22b
notageek-guy <manishkumar97430@gmail.com>
2022-08-31 05:46:27 +0530

    setup express server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("hello");
+// starting server
+const express = require('express');
+const app = express();
+
+
+// first line gives you access to the express library by searching your node modules 
+// next create object
+
+
+app.listen(8000,async()=> {
+    console.log("Server is running ")
+})
