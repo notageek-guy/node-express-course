1c0815e10e0f1c70195d2074671bb2629cadab5d
notageek-guy <manishkumar97430@gmail.com>
2022-08-31 05:51:02 +0530

    add first get route

--- server.js
+++ server.js
@@ -5,8 +5,20 @@ const app = express();
 
 // first line gives you access to the express library by searching your node modules 
 // next create object
+// adding json data to our file 
 
+const mockData = [
+    {name:'Manish'},
+    {name:'Kishan'}
+]
 
+app.get('/',(req,res)=> {
+    res.json({
+        success:true,
+        message: 'succesfully got users Nice',
+        users: mockData
+    })
+})
 app.listen(8000,async()=> {
     console.log("Server is running ")
 })
